d8258752b69aa10b294c14976a1904f93fa96a22
angryWhy <875978822@qq.com>
2022-06-08 21:51:01 +0800

    base

---
+++ pages/recommend/index.js
@@ -0,0 +1,99 @@
+// pages/recommend/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        recommend:[
+            {
+                title:"官方推荐",
+                imgSrc:"../../assets/img/home/编组 9@2x.png"
+            },
+            {
+                title:"中少专区",
+                imgSrc:"../../assets/img/home/编组 10@2x.png"
+            },
+            {
+                title:"哄睡专区",
+                imgSrc:"../../assets/img/home/编组 11@2x.png"
+            },
+            {
+                title:"绘本定制",
+                imgSrc:"../../assets/img/home/编组 12@2x.png"
+            },
+            {
+                title:"成长故事",
+                imgSrc:"../../assets/img/home/编组 13@2x.png"
+            },
+            {
+                title:"艺术启蒙",
+                imgSrc:"../../assets/img/home/编组 13@2x.png"
+            },
+            {
+                title:"恐龙大王",
+                imgSrc:"../../assets/img/home/编组 13@2x.png"
+            },
+            {
+                title:"成长伙伴",
+                imgSrc:"../../assets/img/home/编组 16@2x.png"
+            }
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
