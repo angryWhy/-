12aaae03644ce7d793ce7f0903878083cfa049b6
angryWhy <875978822@qq.com>
2022-06-09 10:15:12 +0800

    recommend

--- pages/recommend/index.js
+++ pages/recommend/index.js
@@ -5,6 +5,7 @@ Page({
      * 页面的初始数据
      */
     data: {
+        // 8个专区
         recommend:[
             {
                 title:"官方推荐",
@@ -39,6 +40,55 @@ Page({
                 imgSrc:"../../assets/img/home/编组 16@2x.png"
             }
         ],
+        // 热门推荐每个选项，
+        recommendItemList:[
+            {
+               src:"../../assets/img/home/位图_4@2x.png",
+               desc:"简笔画第3季游…" 
+            },
+            {
+                src:"../../assets/img/home/位图_2@2x.png",
+                desc:"植物大战僵尸之…"
+            },
+            {
+                src:"../../assets/img/home/位图_3@2x.png",
+                desc:"双语乐读之中英…"
+            },
+            {
+                src:"../../assets/img/home/位图备份 3@2x.png",
+                desc:"我有一只霸王龙…"
+            },
+            {
+                src:"../../assets/img/home/位图备份.png",
+                desc:"简笔画第2季笔下…"
+            },
+            {
+                src:"../../assets/img/home/位图备份 2@2x.png",
+                desc:"睡前听伊索讲故…"
+            }
+        ],
+        popularSaleList:[
+            {
+                src:"../../assets/img/home/popular1.png",
+                desc:"十二生肖奇遇记",
+                simpleDesc:"小动物们的奇遇"
+            },
+            {
+                src:"../../assets/img/home/popular2.png",
+                desc:"十二生肖上学记",
+                simpleDesc:"可可爱爱的小动物们上学啦"
+            },
+            {
+                src:"../../assets/img/home/popular3.png",
+                desc:"儿童画民间故事",
+                simpleDesc:"感受传统民间故事的魅力"
+            },
+            {
+                src:"../../assets/img/home/popular4.png",
+                desc:"伊索寓言第1话",
+                simpleDesc:"看绘本学知识"
+            }
+        ]
     },
 
     /**
