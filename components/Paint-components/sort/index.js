2b2e8699a9e2a6c687afe554784a8aec5627d645
angryWhy <875978822@qq.com>
2022-06-09 11:01:28 +0800

    paint

---
+++ components/Paint-components/sort/index.js
@@ -0,0 +1,26 @@
+// components/Paint-components/sortAge/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        sortItem:{
+            type:Array,
+            vlaue:[]
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
